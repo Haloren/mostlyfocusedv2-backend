d2af023eab443dbf1eb514347c29ae3e88246d1f
Haloren <mckennakyler@gmail.com>
2021-01-24 18:09:22 -0700

    set up todos and events contorollers

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,44 @@
 class EventsController < ApplicationController
+    before_action :set_user
+
+    def index
+        events = Event.all
+        render json: events
+    end
+
+    def show
+        event = Event.find_by(id: params[:id])
+        render json: event
+    end
+
+    def create
+        # event = Event.new(event_params)
+        event = @user.events.new(event_params)
+        if event.save
+            # render json: event
+            render json: @user
+        else
+            render json: {message: event.errors.full_messages.to_sentence}
+        end
+    end
+
+    def destroy
+        event = Event.all.find_by(id: params[:id])
+        user = User.find(event.user_id)
+        if event
+            event.destroy
+            render json: user
+        else 
+            render json: {message: "Delete failed"}
+        end
+    end
+
+    private
+    def set_user
+        @user = User.find(params[:user_id])
+    end
+
+    def event_params
+        params.require(:event).permit(:title, :date, :user_id)
+    end
 end
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,45 @@
 class TodosController < ApplicationController
+    before_action :set_user
+
+    def index
+        todos = Todo.all
+        render json: todos
+    end
+
+    def show
+        todo = Todo.find_by(id: params[:id])
+        render json: todo
+    end
+
+    def create
+        # todo = Todo.new(todo_params)
+        todo = @user.todos.new(todo_params)
+        if todo.save
+            # render json: todo
+            render json: @user
+        else
+            render json: {message: todo.errors.full_messages.to_sentence}
+        end
+    end
+
+    def destroy
+        # debugger;
+        todo = Todo.all.find_by(id: params[:id])
+        user = User.find(todo.user_id)
+        if todo
+            todo.destroy
+            render json: user
+        else 
+            render json: {message: "Delete failed"}
+        end
+    end
+
+    private
+    def set_user
+        @user = User.find(params[:user_id])
+    end
+
+    def todo_params
+        params.require(:todo).permit(:item, :user_id)
+    end
 end
